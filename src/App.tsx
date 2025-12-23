b334017117b9c9095bf82a224cd440101236944d
BuchtaMakova <dajula44@gmail.com>
2025-12-23 21:41:45 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import "./App.css";
+import SchoolDetail from "./components/schoolDetail/SchoolDetail";
+import Timeline from "./components/timeline/Timeline";
+
+function App() {
+  const [activeSchool, setActiveSchool] = useState<number>(1);
+
+  const handleSchoolState = (schoolId: number) => {
+    setActiveSchool(schoolId);
+  };
+  return (
+    <main className="min-h-screen bg-background text-secondary overflow-hidden">
+      {/* Header */}
+      <header className="absolute top-0 left-0 w-full z-30">
+        <div className="max-w-6xl mx-auto px-6 lg:px-10 py-6 flex items-center justify-between text-sm tracking-widest">
+          <div className="font-semibold">
+            <b>DAVID</b>WEB
+          </div>
+          <nav className="space-x-8">
+            <a className="hover:opacity-70" href="#about">
+              ABOUT
+            </a>
+            <a className="hover:opacity-70" href="#gallery">
+              GALLERY
+            </a>
+            <a className="hover:opacity-70" href="#work">
+              WORK
+            </a>
+            <a className="hover:opacity-70" href="#contact">
+              CONTACT
+            </a>
+          </nav>
+        </div>
+      </header>
+
+      {/* Hero */}
+      <section className="relative min-h-screen flex items-center">
+        {/* Shared container */}
+        <div className="relative w-full max-w-6xl mx-auto px-6 lg:px-10">
+          <div className="relative w-full">
+            {/* Image */}
+            <div className="absolute right-0 top-1/2 -translate-y-1/2 w-[70%] aspect-[3/2] z-0">
+              <img
+                src="/profile.jpg"
+                alt="Portrait"
+                className="object-cover grayscale w-full h-full"
+              />
+            </div>
+
+            {/* Content */}
+            <div className="relative z-10">
+              <h1 className="text-5xl lg:text-6xl font-bold leading-tight mb-6">
+                Hello,
+                <br />
+                I’m David Urban
+              </h1>
+
+              <p className="max-w-md leading-relaxed">
+                I’m a tech enthusiast and musician who loves programming,
+                playing guitar, and exploring creative projects. When I’m not
+                coding or making music, I’m into gaming, scale modeling, and
+                watches. Currently, I’m in L’Aquila for Erasmus, expanding my
+                horizons. Welcome to my space!
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section
+        className="flex h-screen z-[10] justify-center items-center overflow-hidden"
+        id="Education"
+      >
+        <div className="flex relative items-center max-w-6xl mx-auto h-3/4 justify-between  px-6 lg:px-10 w-full">
+          <div className="flex h-full w-3/8 items-center">
+            <Timeline updateSchool={handleSchoolState} />
+          </div>
+
+          <div className="flex h-full aspect-square">
+            <SchoolDetail schoolId={activeSchool} />
+          </div>
+        </div>
+        <div className="absolute w-[600px] h-[600px] bg-primary rounded-full right-[65%] bottom-100 transform translate-x-[50%] translate-y-[50%] z-[-1] blur-[100px] opacity-50" />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/timeline/Timeline.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./Timeline.css";
+import highschool from "@/assets/images/frengplogo.png";
+import college from "@/assets/images/osulogo.png";
+import erasmus from "@/assets/images/univaqlogo2.png";
+
+interface Props {
+  updateSchool: (schoolState: number) => void;
+}
+
+const Timeline = ({ updateSchool }: Props) => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(0);
+
+  const handleClick = (index: number) => {
+    setActiveIndex(index);
+    updateSchool(index + 1);
+  };
+
+  const events = [
+    { name: "High School", image: highschool },
+    { name: "College", image: college },
+    { name: "Erasmus", image: erasmus },
+  ];
+
+  return (
+    <div className=" min-w-[290px] flex flex-1 flex-col items-center timeline-container  justify-center h-full p-7 ">
+      {events.map((event, index) => {
+        const isEven = index % 2 === 0; // Even index = left, Odd index = right
+
+        return (
+          <div key={index} className="flex w-full items-center h-1/3">
+            {/*Left*/}
+            <div className="flex-1 text-right">
+              {!isEven && <h3 className="text-lg font-bold">{event.name}</h3>}
+            </div>
+
+            {/*Middle*/}
+            <div className="flex flex-1 flex-col items-center h-full relative timeline">
+              <div
+                className={`circle ${activeIndex === index ? "active" : ""}`}
+                style={{ backgroundImage: `url(${event.image})` }}
+                onClick={() => handleClick(index)}
+              ></div>
+
+              <div
+                className={`line ${activeIndex === index ? "active" : ""}`}
+              ></div>
+            </div>
+
+            {/*Right*/}
+            <div className="flex-1 text-left">
+              {isEven && <h3 className="text-lg font-bold">{event.name}</h3>}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Timeline;
