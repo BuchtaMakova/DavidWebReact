78d6bbdec4430ae1452712ad5f32f4d5748cfcfc
BuchtaMakova <dajula44@gmail.com>
2026-02-24 12:47:44 +0100

    fix: update API endpoint in ContactForm to use HTTPS

--- src/components/form/ContactForm.tsx
+++ src/components/form/ContactForm.tsx
@@ -26,7 +26,7 @@ export const ContactForm = () => {
     setIsSending(true);
 
     try {
-      await fetch("http://davidweb.runasp.net/api/contact", {
+      await fetch("https://davidweb.runasp.net/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, message }),
